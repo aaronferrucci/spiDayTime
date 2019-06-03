56de0d7d08b0017f16704c0c8795f71af83bebd5
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2019-06-02 20:38:42 -0700

    Use real time of day (seconds in day)

--- spiDayTime.c
+++ spiDayTime.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
@@ -17,6 +18,28 @@ int spiSetup(int chan, int speed)
   return fd;
 }
 
+unsigned int get_seconds_in_day()
+{
+  unsigned int secs = 0;
+  time_t rawtime;
+  struct tm *info;
+  time(&rawtime);
+  info = localtime(&rawtime);
+
+  secs += info->tm_sec;
+  secs += info->tm_min * 60;
+  secs += info->tm_hour * 60 * 60;
+  
+  return secs;
+}
+
+void spi_encode(unsigned int seconds_in_day, unsigned char daytime[3])
+{
+  daytime[0] = (seconds_in_day & 0xFF0000) >> 16;
+  daytime[1] = (seconds_in_day & 0xFF00) >> 8;
+  daytime[2] = seconds_in_day & 0xFF;
+}
+
 int main(void)
 {
   int fd;
@@ -25,12 +48,10 @@ int main(void)
 
   wiringPiSetup();
   fd = spiSetup(spi_chan, 1);
-  int fake_datetime = 0;
   while (1) {
+    unsigned int seconds_in_day = get_seconds_in_day();
     // MS byte first
-    daytime[0] = (fake_datetime & 0xFF0000) >> 16;
-    daytime[1] = (fake_datetime & 0xFF00) >> 8;
-    daytime[2] = fake_datetime & 0xFF;
+    spi_encode(seconds_in_day, daytime);
 
     // the buffer is transmitted index-0-first
     // bytes are transmitted MSB first.
@@ -38,9 +59,9 @@ int main(void)
       printf("SPI failure: %s\n", strerror(errno));
       break;
     }
+    printf("txdata: %d\n", seconds_in_day);
 
-    // delay(100);
-    ++fake_datetime;
+    delay(10000);
   }
 
   close(fd);
